91840ed3e6655dcb2452e4ac617f91508465de49
binamayghale5 <binamayaghale5@gmail.com>
2025-07-18 22:40:49 +0545

    Date validation in js

--- js/apply.js
+++ js/apply.js
@@ -5,4 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.getElementById('jobTitle').value = job;
   document.getElementById('location').value = location;
+
+  // Set applyDate to today
+  const dateInput = document.getElementById('applyDate');
+  if (dateInput) {
+    const today = new Date().toISOString().split('T')[0]; // yyyy-mm-dd format
+    dateInput.value = today;
+  }
 });
